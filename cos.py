677200c4ce6119f687b0b61ad265a7c6ccb82fa9
pratv3:~# <74216667+pratv3@users.noreply.github.com>
2021-09-12 16:16:09 +0530

    Check it out

---
+++ cos.py
@@ -0,0 +1,24 @@
+from pytube import YouTube
+
+print("--------------------url----------------")
+k=input("what is your url of the video::")
+if "https"or "http" in k :
+    try:
+        print(k)
+        video=YouTube(k)
+        print("########################TITLE##############")
+        print(video.title)
+        print("_________________________thumbnail image___________________")
+        print(video.thumbnail_url)
+        print("______________________Download video_________________________________")
+        print("!!!!!!!!!!!!!!!!!Download!!!!!!!!!!!!!!!!!!")
+        #for stream in video.streams:
+            #print(stream)
+        video=video.streams.get_highest_resolution()
+        video.download()
+        print("sucess")
+    except Exception as e:
+        print("somthing is going wrong the webpage may be removed or under maintanence")
+        print(e)
+else:
+    print("invalid address!")
